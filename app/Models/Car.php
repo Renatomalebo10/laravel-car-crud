0cf8dc419cf45285b285d8c234d2993e93465a00
Renato Malebo <renatomalebo10@gmail.com>
2026-08-28 09:41:41 +0100

    feat: adiciona campo de cor em select predefinido nos modais de cadastramento e edicao

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,19 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Car extends Model
 {
-    use HasFactory;
+    protected $fillable = ['category_id', 'marca', 'modelo', 'cor', 'ano', 'placa', 'preco'];
 
-    protected $fillable = [
-        'marca',
-        'modelo',
-        'ano',
-        'placa',
-        'cor',
-        'preco',
-    ];
+    // Um carro pertence a uma categoria
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
