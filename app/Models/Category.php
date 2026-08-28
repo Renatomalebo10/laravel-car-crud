5e6d7efcef4c271942fb655b3108a07bea08e1ab
Renato Malebo <renatomalebo10@gmail.com>
2026-08-28 09:41:42 +0100

    feat: implementa relacionamento 1:N entre categorias e carros no banco de dados e na interface

---
+++ app/Models/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    protected $fillable = ['nome', 'descricao'];
+
+    // Uma categoria tem muitos carros
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
+    }
+}
