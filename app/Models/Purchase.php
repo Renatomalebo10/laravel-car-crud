52868295bce0dd9a5777718b0adca75963addd38
Renato Malebo <renatomalebo10@gmail.com>
2026-09-08 15:25:00 +0100

    feat(models): cria modelo Purchase

---
+++ app/Models/Purchase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'car_id',
+        'preco_compra',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
+}
